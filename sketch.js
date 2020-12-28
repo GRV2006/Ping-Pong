ae1ac74dee330437ecdc57c7496837029d30eafc
GRV2006 <76687193+GRV2006@users.noreply.github.com>
2020-12-28 16:48:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(222, 0, 123)
+  rectMode('center')
+  fill('purple')
+  rect(200, 393, 50, 7)
+  fill('pink')
+  rect(mouseX, 0, 50, 7)
+  fill('blue')
+  rect(200, 200, 8, 8)
+  stroke('yellow')
+  fill('yellow')
+  text('PING PONG', 173, 209)
+}
+
+
+  
+  
